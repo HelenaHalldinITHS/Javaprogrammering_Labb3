947f2ad7a881b89c32955ecc1d0732067d4f7725
HelenaHalldinITHS <helena.halldin@iths.se>
2021-10-21 09:41:39 +0200

    Makes changes in css file and fxml

--- src/main/java/se/iths/helena/javafx/labb3/DrawingController.java
+++ src/main/java/se/iths/helena/javafx/labb3/DrawingController.java
@@ -1,8 +1,33 @@
 package se.iths.helena.javafx.labb3;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.text.Text;
 
 public class DrawingController {
+
+    @FXML
+    private Button circleButton;
+    @FXML
+    private Button rectangleButton;
+    @FXML
+    private Button triangleButton;
+
+    @FXML
+    private ColorPicker colorPicker;
+    @FXML
+    private Slider sizeSlider;
+    @FXML
+    private Label sizeLabel;
+    @FXML
+    private Text sizeText;
+
+    @FXML
+    private Button undoButton;
+    @FXML
+    private Button modeButton;
 
 }
