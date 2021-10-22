9fd13826e3068bffeaee0bc3133f7a991328a7a4
HelenaHalldinITHS <helena.halldin@iths.se>
2021-10-22 15:34:11 +0200

    Changes currentShape from string object to enum

--- src/main/java/se/iths/helena/javafx/labb3/ShapeFactory.java
+++ src/main/java/se/iths/helena/javafx/labb3/ShapeFactory.java
@@ -3,7 +3,6 @@ package se.iths.helena.javafx.labb3;
 
 
 public class ShapeFactory {
-
     public Shape getShape(ShapeType shapeType) {
         return switch (shapeType) {
             case Circle -> new Circle();
