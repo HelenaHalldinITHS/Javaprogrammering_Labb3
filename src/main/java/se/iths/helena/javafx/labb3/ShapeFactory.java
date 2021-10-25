b39c8c05f03bd0e07bb65bbb0af931222578531e
HelenaHalldinITHS <helena.halldin@iths.se>
2021-10-25 11:40:21 +0200

    adds factory method for getting shape from parameters

--- src/main/java/se/iths/helena/javafx/labb3/ShapeFactory.java
+++ src/main/java/se/iths/helena/javafx/labb3/ShapeFactory.java
@@ -1,12 +1,22 @@
 package se.iths.helena.javafx.labb3;
 
 
+import javafx.scene.paint.Color;
 
 public class ShapeFactory {
-    public Shape getShape(ShapeType shapeType) {
+    public static Shape getShape(ShapeType shapeType) {
         return switch (shapeType) {
             case Circle -> new Circle();
             case Rectangle -> new Rectangle();
         };
     }
+
+    public static Shape getShape(ShapeType shapeType, Color color, double size, double x, double y) {
+        return switch (shapeType) {
+            case Circle -> new Circle(color, size, x, y);
+            case Rectangle -> new Rectangle(color, size, x, y);
+        };
+    }
+
+
 }
